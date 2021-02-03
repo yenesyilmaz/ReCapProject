0cda75bb59df642907d3d64269328e88197233a4
Enes Yılmaz <yenesyilmaz11@hotmail.com>
2021-02-03 19:16:17 +0300

    ConsoleUI

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
+using System.Collections.Generic;
 
 namespace ConsoleUI
 {
@@ -11,20 +13,34 @@ namespace ConsoleUI
         {
             CarManager carManager = new CarManager(new InMemoryCarDal());
             BrandManager brandManager = new BrandManager(new InMemoryBrandDal());
-            
 
-            foreach (var brand in brandManager.GetBrands())
+            foreach (var cars in carManager.GetCars())
             {
-                Console.WriteLine("{0} = {1}",brand.BrandId,brand.BrandName);
+                foreach (var brands in brandManager.GetBrands())
+                {
+                    if (cars.BrandId == brands.BrandId)
+                    {
+                        Console.WriteLine("{0} model {1} {2} günlük ücreti {3}",cars.ModelYear,brands.BrandName,cars.Description,cars.DaliyPrice);
+                    }
+                }
             }
 
-            
-            
 
-            foreach (var car in carManager.GetCars())
-            {
-                Console.WriteLine(car.ModelYear);
-            }
+            
+            
+            
+           
+
+            //foreach (var brand in brandManager.GetBrands())
+            //{
+            //    Console.WriteLine("{0} = {1}",brand.BrandId,brand.BrandName);
+            //}
+
+          
+            //foreach (var car in carManager.GetCars())
+            //{
+            //    Console.WriteLine(car.ModelYear);
+            //}
 
         }
     }
